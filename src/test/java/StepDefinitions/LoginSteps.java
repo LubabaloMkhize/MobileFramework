a82d1988f0fc25f2ca7f8fb2f625890ca8635c4a
lubabalo.mkhize <Lubabalo.Mkhize@gmail.com>
2026-03-12 12:40:04 +0200

    Added login steps class.

---
+++ src/test/java/StepDefinitions/LoginSteps.java
@@ -0,0 +1,32 @@
+package StepDefinitions;
+
+import Utils.Base;
+import io.cucumber.java.en.*;
+
+public class LoginSteps extends Base {
+    @Given("the user is on the login screen")
+    public void the_user_is_on_the_login_screen() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the user enters username {string}")
+    public void the_user_enters_username(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @And("the user enters password {string}")
+    public void the_user_enters_password(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @And("the user taps the login button")
+    public void the_user_taps_the_login_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the user should be logged into the mobile application")
+    public void the_user_should_be_logged_into_the_mobile_application() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
